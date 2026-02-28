3e79e1b273fd3c804b038323a89fe0386a82ae60
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-28 12:58:27 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
+def add(a,b):
+    return(a+b)
+   
+
+def subtract(a,b):
+    return(a-b)
+  
+
+def multiply(a,b):
+    return(a*b)
+   
+
+def divide(a,b):
+    if b ==0:
+        return "Giving an error!divided by zero"
+    return(a/b)
+
 print("1 - Add")
 print("2 - Subtract")
-print("3 - Multiply")
-print("4 - Divide")
+print("3 - multiply")
+print("4 - divide")
 option = int(input("choose an option: "))
 
 if option in [1,2,3,4,]:
@@ -11,17 +28,16 @@ if option in [1,2,3,4,]:
     num2 = float(input("Enter your second number: "))  
 
     if (option ==1):
-        result = num1 +num2
-
+       result = add(num1, num2)
 
     elif(option ==2):
-        result = num1 - num2
+        result = subtract(num1, num2)
 
     elif(option ==3):
-        result = num1*num2
+        result = multiply(num1, num2)
 
     elif(option ==4):
-        result = num1/num2
+        result = divide(num1, num2)
 
     print("The result of the opreation is {}" .format(result))
 
